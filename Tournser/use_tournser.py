c777ae9b0d9cca5ded3d0f68e1dbbe35e5645a9a
OliviaMcGough <mcgougho@reed.edu>
2023-04-11 12:23:32 -0700

    add usage

--- Tournser/use_tournser.py
+++ Tournser/use_tournser.py
@@ -40,14 +40,3 @@ def make_tournaplex_input(df, year, threshold):
 
 
 
-# USING TOURNSER 
-
-
-# go to directory where tournser is stored
-# run the following code
-# input.txt is formatted as above
-# output.txt is the file you want to save the persistence intervals, cell counts, and betti numbers,
-# extra.txt is the file you want to save the tournaple, formatted as  described in in tournser_documentation.pdf from https://github.com/JasonPSmith/tournser.git
-./tournser input.txt output.txt --print extra.txt
-# if you don't want the tournaplex printed, don't write '--print extra.txt'. 
-# you can also specifiy filtrations, see tournser_documentation.pdf from https://github.com/JasonPSmith/tournser.git
